206ebad458741c0fa539c764d9d0664f3f9ce479
phucgialongnguyen-ux <phucgialongnguyen@gmail.com>
2026-08-30 10:28:05 +0000

    myOS

--- kernel.cpp
+++ kernel.cpp
@@ -226,11 +226,11 @@ extern "C" void kernel_main() {
     print << "Write something! \n";
     print.RestoreColor();
     
-    while(1){
-        char text = Screen::Keyboard_Driver();
-        if(text != 0){
-            char str[2] = {text, '\0'};
-            print << str;               
-        }
-    }
+//    while(1){
+//        char text = Screen::Keyboard_Driver();
+//        if(text != 0){
+//            char str[2] = {text, '\0'};
+//            print << str;               
+//       }
+//    }
 }
