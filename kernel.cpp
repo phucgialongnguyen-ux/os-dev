cace8e961b380a492fde8629bea3e46cb65bc11c
phucgialongnguyen-ux <phucgialongnguyen@gmail.com>
2026-09-03 04:14:46 +0000

    yoursOS

--- kernel.cpp
+++ kernel.cpp
@@ -329,7 +329,9 @@ class Screen{
                         if(data != 0xFFFFFFFF){
                             unsigned short vendor_id = (unsigned short)(data & 0xFFFF);
                             unsigned short device_id = (unsigned short)((data >> 16) & 0xFFFF);
-                            *this << "Vendor: 0x" << vendor_id << ", Device: 0x" << device_id << "\n";
+                            if(vendor_id == 0x8086 && device_id == 0x100F){
+                                *this << "Vendor: 0x" << vendor_id << ", Device: 0x" << device_id << "\n";
+                            }
                         }
                     }
                 }
